aee8a657a85725ab36b636fb2562f608640a4eac
Epalma98 <elijahpalma98@yahoo.com>
2023-05-09 16:12:33 -0700

    Added react app files

---
+++ my-react-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PortfolioContainer from './components/PortfolioContainer';
+
+ReactDOM.render(<PortfolioContainer />, document.getElementById('root'));
